4c2a72a986c6694fe6fe982b883c77d54275eaa0
Osarumwense <osaru97@yahoo.com>
2017-06-19 02:26:22 -0400

    added database configurations for Mongo and added schema definitions

--- internHub/hub/models.py
+++ internHub/hub/models.py
@@ -2,4 +2,30 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from djangotoolbox.fields import ListField
+
 # Create your models here.
+
+class Job(models.Model):
+    jobId = models.IntegerField()
+    title = models.CharField()
+    description = models.TextField()
+    tags = ListField()
+    employer = CharField()
+
+class Employer(models.Model):
+    employerId = models.IntegerField()
+    name = models.CharField()
+    tags = ListField()
+
+class Student(models.Model):
+    studentId = models.IntegerField()
+    lastName = models.CharField()
+    firstName = models.CharField()
+    schoolId = models.IntegerField()
+
+class School(models.Model):
+    schoolId = models.IntegerField()
+    name = models.CharField()
+    coordinators = ListField()
+    
--- internHub/internHub/settings.py
+++ internHub/internHub/settings.py
@@ -74,10 +74,10 @@ WSGI_APPLICATION = 'internHub.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default' : {
+      'ENGINE' : 'django_mongodb_engine',
+      'NAME' : 'my_database'
+   }
 }
 
 
