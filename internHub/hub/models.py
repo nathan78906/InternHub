343ca7ab341990d17a639c7724315eabc4bb18de
nathan78906 <nathan.seebarran@mail.utoronto.ca>
2017-06-27 12:35:04 -0400

    implemented django templates for front end

--- internHub/hub/models.py
+++ internHub/hub/models.py
@@ -1,31 +1,22 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
-
 class Job(models.Model):
-    jobId = models.IntegerField()
     title = models.CharField(max_length=100)
     description = models.TextField()
-    junior = models.BooleanField()
-    intermediate = models.BooleanField()
-    senior = models.BooleanField()
+    SKILL_CHOICES = (
+        ('J', 'Junior'),
+        ('I', 'Intermediate'),
+        ('S', 'Senior'),
+    )
+    skill = models.CharField(max_length=1, choices=SKILL_CHOICES)
     employer = models.CharField(max_length=100)
+	
+    #@python_2_unicode_compatible
+    def __str__(self):
+        return self.title + " - " + self.employer
 
-class Employer(models.Model):
-    employerId = models.IntegerField()
-    name = models.CharField(max_length=50)
-    tags = models.CharField(max_length=20)
-
-class Student(models.Model):
-    studentId = models.IntegerField()
-    lastName = models.CharField(max_length=30)
-    firstName = models.CharField(max_length=30)
-    schoolId = models.IntegerField()
-
-class School(models.Model):
-    schoolId = models.IntegerField()
-    name = models.CharField(max_length=70)
-    coordinator = models.CharField(max_length=50)
-    
--- internHub/hub/views.py
+++ internHub/hub/views.py
@@ -1,7 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+from .models import Job
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the hub index.")
-    
+    job_list = Job.objects.all()
+    context = {
+        'job_list': job_list,
+    }
+    return render(request, 'hub/index.html', context)
+
+def detail(request, job_id):
+    return HttpResponse("You're looking at job %s." % job_id)
