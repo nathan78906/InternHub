37f33d5a08f5ebc99fb79a54b8eb89cd81a63269
nathan78906 <nathen78906@gmail.com>
2017-07-23 00:36:49 -0400

    added settings and static

--- internHub/internHub/urls.py
+++ internHub/internHub/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from material.frontend import urls as frontend_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^hub/', include('hub.urls')),
